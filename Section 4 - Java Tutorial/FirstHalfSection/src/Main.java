b7fe6a58b060f2bab6847012a321ce767227fbbe
Ming Chen <mingchen0321@gmail.com>
2022-07-22 22:54:51 -0400

    created code files for first half of the class section

---
+++ Section 4 - Java Tutorial/FirstHalfSection/src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // Video 44. Keywords And Expression
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+        // Video 45. Statements, Whitespace and Indentation
+        int myVariable = 50;
+        myVariable++;
+
+        // Video 46. Code Blocks And the If Then Else statement
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if (score < 5000) {
+//            System.out.println("You score was less than 5000");
+//        } else if (score < 1000) {
+//            System.out.println("You score was less than 1000");
+//        } else {
+//            System.out.println("Got here");
+//        }
+        if (gameOver == true) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        int secondScore = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+            int finalScore = secondScore + (levelCompleted * bonus);
+            System.out.println(("You second final score was " + finalScore));
+        }
+
+
+    }
+}
