f13ca71485ae90bc110e4dafb597d9d9d3ac7155
Ming Chen <mingchen0321@gmail.com>
2022-07-23 20:57:39 -0400

    create Code Excercise folder and add SpeedConverter code

---
+++ Section 4 - Java Tutorial/CodeExcecise/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double speed1 = 1.5;
+        double speed2 = 10.25;
+        double speed3 = -5.6;
+        double speed4 = 25.42;
+        double speed5 = 75.114;
+        SpeedConverter.printConversion(speed1);
+        SpeedConverter.printConversion(speed2);
+        SpeedConverter.printConversion(speed3);
+        SpeedConverter.printConversion(speed4);
+        SpeedConverter.printConversion(speed5);
+    }
+}
