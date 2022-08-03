7adf0fabe89019eba8077ac34ff1b9c1079ecfe6
Ming Chen <mingchen0321@gmail.com>
2022-08-02 21:02:26 -0400

    add class code for section 6

---
+++ Section 6 - OOP Part 1/ClassCode/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // --------------------------------------Video 77 & 78. Classes  -------------------------------
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.printf("Model is " + porsche.getModel());
+    }
+}
