eb6f663f6cb8a44c8f44ae6bc8704b7772ea1c5c
Ming Chen <mingchen0321@gmail.com>
2022-08-06 17:07:53 -0400

    add class code

--- Section 6 - OOP Part 1/ClassCode/src/Main.java
+++ Section 6 - OOP Part 1/ClassCode/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         Car porsche = new Car();
         Car holden = new Car();
         porsche.setModel("Carrera");
-        System.out.printf("Model is " + porsche.getModel());
+        System.out.println("Model is " + porsche.getModel());
+
+        // --------------------------------------Video 81. Inheritance  -------------------------------
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+        Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+        dog.eat();
     }
 }
