8420b3d96bc7408ecbbdd56339f103ca3e8a47e2
Ming Chen <mingchen0321@gmail.com>
2022-08-14 11:51:28 -0400

    add code files to exercise questions

---
+++ Section 7 - OOP Part 2/ExerciseCode/src/Hamburger.java
@@ -0,0 +1,15 @@
+public class Hamburger {
+    private String name;
+    private String meat;
+    private String breadRollType;
+    private double price;
+
+    public Hamburger(String name, String meat, String breadRollType, double price) {
+        this.name = name;
+        this.meat = meat;
+        this.breadRollType = breadRollType;
+        this.price = price;
+    }
+
+    
+}
